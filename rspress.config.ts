f06d7bfd4606d4a791de5a0f4e646f33bf6a0833
plasticine9750 <975036719@qq.com>
2023-11-17 22:41:46 +0800

    build: set base to /rspress-demo/

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,18 +1,17 @@
-import * as path from 'path';
-import { defineConfig } from 'rspress/config';
+import * as path from 'path'
+import { defineConfig } from 'rspress/config'
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
+  base: '/rspress-demo/',
   title: 'Rspress Demo',
   description: 'Rspack-based Static Site Generator',
-  icon: "/rspress-icon.png",
+  icon: '/rspress-icon.png',
   logo: {
-    light: "/rspress-light-logo.png",
-    dark: "/rspress-dark-logo.png",
+    light: '/rspress-light-logo.png',
+    dark: '/rspress-dark-logo.png',
   },
   themeConfig: {
-    socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
-    ],
+    socialLinks: [{ icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' }],
   },
-});
+})
